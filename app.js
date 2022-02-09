ea9bbf988b5230953a47082ea98430d5271812d7
rosauracotos <rosacotossullon@gmail.com>
2022-02-08 22:24:52 -0500

    primer avance trivia

---
+++ app.js
@@ -0,0 +1,5 @@
+let empezarJuego=document.getElementById('jugar').addEventListener('click',empezar);
+function empezar(){
+    console.log("hola boton");
+    document.getElementById('formularios').style.display='block';
+}
